47ad5969f31a7a7990cce0ba2eb4d010f880c7ed
strand <strand@bettermistak.es>
2014-10-07 10:49:01 -0700

    Create create and new actions for Elephant.
    - Create a route for create and new in the controller.
    - Create a template for the new form.

--- app/controllers/elephants_controller.rb
+++ app/controllers/elephants_controller.rb
@@ -5,6 +5,18 @@ get '/elephants' do
   erb :elephants_index
 end
 
+# new elephant - Technically not in REST
+get '/elephants/new' do
+  erb :elephants_new
+end
+
+# Create the elephant in the Database - Elements POST
+post '/elephants' do
+  Elephant.create params
+
+  redirect '/elephants'
+end
+
 ## Show - Element's GET
 get '/elephants/:id' do
   @elephant = Elephant.find params[:id]
